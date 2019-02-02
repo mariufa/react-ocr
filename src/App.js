5ca7ab87db88a7f56276146caf4feb5682fb8260
Marius Fagerland <mariusfagerland@yahoo.no>
2019-02-02 14:46:22 +0100

    Create mvp upload file app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,52 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+
+import styled from 'styled-components';
+
+
+const FileInput = styled.input`
+  margin: auto;
+  padding: 2em;
+  display: block;
+`;
+
+const TextBox = styled.div`
+  margin: 1em auto;
+  width: 80%;
+  display: block;
+`;
 
 class App extends Component {
+
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      ocrResult: ''
+    }
+  }
+
+  takePicture = e => {
+    let picture = e.target.files[0]
+    console.log(picture)
+    const data = new FormData();
+    data.append('file', picture, picture.name);
+    fetch('http://python-ocr.herokuapp.com/', {
+      method: 'POST',
+      body: data,
+    })
+    .then(response => response.text())
+    .then(data => this.setState({ocrResult: data}));
+  }
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div>
+        <FileInput type="file" accept="image/*;capture=camera" onChange={this.takePicture}></FileInput>
+        <TextBox>
+          {this.state.ocrResult}  
+        </TextBox>
       </div>
     );
   }
